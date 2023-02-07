594ae11ff8da49d47b6585d0a456e3c5027e3413
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-07 18:08:36 -0500

    chore: setup project wiht vite + react ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Routes } from 'react-router-dom'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      
+    </Routes>
+  )
+}
+
+export default App
